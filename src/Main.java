dab1bec625e2f18b04de207e9e547ff7af13d98d
Sebastian Tejedor Montiel <stejedorm@unicartagena.edu.co>
2026-05-14 12:55:21 -0500

    Actualizando mensaje de bienvenida

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Sebastian Tejedor"));
+// Ejercicio Git y Github Actividad Unidad2
+//Universidda de Cartagena
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+void main() {
+    String gretting = "Hello!, welcome Back!";
+    String name = "Sebastian Tejedor Montiel";
+    int age = 17;
+    IO.println(gretting + " My name is " + name + " and I am " + age + " years old.");
 }
